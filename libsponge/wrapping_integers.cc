a445e39c4ae83396fef930870a2f3bcde878b7b0
yy158775 <1584616775@qq.com>
2022-10-25 17:14:16 +0800

    finish:lab2-wrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -15,7 +15,8 @@ using namespace std;
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
     DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t b = static_cast<uint32_t>(n%0x100000000);
+    return operator+(isn,b);
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -30,5 +31,20 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
     DUMMY_CODE(n, isn, checkpoint);
+    uint32_t offset = operator-(n,isn);
+    uint64_t startidx = static_cast<uint64_t>(offset);
+    if(checkpoint <= startidx) {
+        return startidx;
+    } else {
+        int64_t dis = static_cast<int64_t>(checkpoint - startidx);
+        int64_t res = dis/(0x100000000);
+        uint64_t left = static_cast<uint64_t>(startidx + res*(0x100000000));
+        uint64_t right = static_cast<uint64_t>(startidx + (res+1)*(0x100000000));
+        if((checkpoint-left) <= static_cast<uint64_t>(INT32_MAX)) {
+            return left;
+        } else if((right-checkpoint) <= static_cast<uint64_t>(INT32_MAX)) {
+            return right;
+        }
+    }
     return {};
 }
